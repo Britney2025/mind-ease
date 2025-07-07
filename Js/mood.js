486399cf84f313abf6f3fc27a1f66d5c92a867f0
Britney2025 <chosenbright1@gmail.com>
2025-07-07 13:31:18 +0300

    add affirmation slider in the mood.js

--- Js/mood.js
+++ Js/mood.js
@@ -25,3 +25,29 @@ if (mood && affirmations[mood]) {
 
     document.getElementById("affirmation").innerText = "Please go back and select how you're feeling💭"
 }
+
+const extraAffirmations = [
+    "You are enough.",
+    "You are doing your best, and that is enough.",
+    "It's okay to rest.",
+    "You are safe in this moment.",
+    "Your feelings are valid.",
+    "You have survived 100% of your hardest days.",
+    "You are allowed to grow at your own pace.",
+    "You bring value to the world simply by being you!"
+];
+
+let currentIndex = 0;
+const textEl = document.getElementById("affirmationText");
+
+function showNextAffirmation() {
+    textEl.style.opacity = 0;
+
+    setTimeout(() => {
+        currentIndex = (currentIndex + 1) % extraAffirmations.length;
+        textEl.textContent = extraAffirmations[currentIndex];
+        textEl.style.opacity = 1;
+    }, 500);
+}
+
+setInterval(showNextAffirmation, 5000);
