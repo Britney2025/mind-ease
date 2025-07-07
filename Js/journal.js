71821f35a80d8aab3ddf2bdb816e15490f86a5dd
Britney2025 <chosenbright1@gmail.com>
2025-07-07 10:06:25 +0300

    add js to the journal page for functionality

---
+++ Js/journal.js
@@ -0,0 +1,45 @@
+function saveJournal() {
+    const text = document.getElementById("journal-text").value.trim();
+
+    if (!text) return;
+    const date = new Date().toLocaleString();
+    const entry = {text, date};
+
+    const entries = JSON.parse(localStorage.getItem("journalEntries")) || [];
+    entries.unshift(entry);
+
+    localStorage.setItem("journalEntries", JSON.stringify(entries));
+
+    document.getElementById("journal-text").value = "";
+    document.getElementById("saveMsg").innerText = "Entry Saved!";
+    setTimeout(() => {
+        document.getElementById("saveMsg").innerText = ""
+    }, 3000);
+    displayEntries();
+}
+
+function displayEntries() {
+    const entries = JSON.parse(localStorage.getItem("journalEntries")) || [];
+
+    const list = document.getElementById("entryList");
+    list.innerHTML = "";
+
+    entries.forEach((entry, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <p>${entry.text}</p>
+        <small>${entry.date}</small>
+        <button onclick="deleteEntry(${index})">🗑 Delete</button>
+        `;
+        list.appendChild(li);
+    });
+}
+
+function deleteEntry(index) {
+    const entries = JSON.parse(localStorage.getItem("journalEntries")) || [];
+    entries.splice(index, 1);
+    localStorage.setItem("journalEntries", JSON.stringify(entries));
+    displayEntries();
+}
+
+displayEntries();
