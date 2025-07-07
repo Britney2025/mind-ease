94521346ab6e4091b069ef6e71c02077473f2d2b
Britney2025 <chosenbright1@gmail.com>
2025-07-07 16:04:25 +0300

    move home.js function to the mood.js for the moods

--- Js/home.js
+++ Js/home.js
@@ -1,13 +1,3 @@
-const moods = document.querySelectorAll('.mood');
-
-moods.forEach(button => {
-    button.addEventListener('click', () => {
-        const selectedMood = button.getAttribute('data-mood');
-        localStorage.setItem('userMood', selectedMood);
-        window.location.href = 'mood.html';
-    });
-});
-
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     sidebar.style.width = sidebar.style.width === "250px" ? "0" : "250px";
--- Js/mood.js
+++ Js/mood.js
@@ -1,3 +1,13 @@
+const moods = document.querySelectorAll('.mood');
+
+moods.forEach(button => {
+    button.addEventListener('click', () => {
+        const selectedMood = button.getAttribute('data-mood');
+        localStorage.setItem('userMood', selectedMood);
+        window.location.href = 'mood.html';
+    });
+});
+
 const mood = localStorage.getItem("userMood");
 
 const moodEmojis = {
